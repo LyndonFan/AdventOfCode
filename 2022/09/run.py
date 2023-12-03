7c6ffec3b71366a9ba270ebe9b60616ac1ec7f7a
Lyndon Fan <lyndon0808@gmail.com>
2023-12-03 15:40:26 +0000

    finish 2022 09 w/ custom class

--- 2022/09/run.py
+++ 2022/09/run.py
@@ -1,7 +1,73 @@
 # year 2022 day 9
 import os
+from typing import Optional
 
 CWD = os.path.dirname(os.path.abspath(__file__))
+
+
+class Node:
+    def __init__(self, x: int, y: int, child: Optional["Node"] = None):
+        self.x = x
+        self.y = y
+        self.child = child
+
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
+    def move(self, target_x, target_y):
+        self.x = target_x
+        self.y = target_y
+        if self.child:
+            self.child.update(self.x, self.y)
+
+    def update(self, parent_x, parent_y):
+        if abs(self.x - parent_x) <= 1 and abs(self.y - parent_y) <= 1:
+            return
+        if self.x == parent_x or self.y == parent_y:
+            if self.x == parent_x:
+                if self.y > parent_y:
+                    self.y -= 1
+                else:
+                    self.y += 1
+            if self.y == parent_y:
+                if self.x > parent_x:
+                    self.x -= 1
+                else:
+                    self.x += 1
+        else:
+            if self.x > parent_x:
+                self.x -= 1
+            else:
+                self.x += 1
+            if self.y > parent_y:
+                self.y -= 1
+            else:
+                self.y += 1
+        if self.child:
+            self.child.update(self.x, self.y)
+
+
+class Chain:
+    def __init__(self, start_x: int, start_y: int, num_nodes: int):
+        self.nodes = [Node(start_x, start_y) for _ in range(num_nodes)]
+        for i in range(1, len(self.nodes)):
+            self.nodes[i - 1].child = self.nodes[i]
+
+    def __repr__(self):
+        return " -> ".join([str(n) for n in self.nodes])
+
+    @property
+    def tail_pos(self) -> tuple[int, int]:
+        return self.nodes[-1].x, self.nodes[-1].y
+
+    def move(self, d: str):
+        d_pos = {
+            "U": (0, 1),
+            "D": (0, -1),
+            "L": (-1, 0),
+            "R": (1, 0),
+        }[d]
+        self.nodes[0].move(self.nodes[0].x + d_pos[0], self.nodes[0].y + d_pos[1])
 
 
 def parse(inp):
@@ -11,107 +77,38 @@ def parse(inp):
     return data
 
 
-def update(newhpos, tpos):
-    nhx, nhy = newhpos
-    tx, ty = tpos
-    if max(abs(nhx - tx), abs(nhy - ty)) <= 1:
-        return tpos
-    if nhx - tx >= 2:
-        tx += 1
-        ty = nhy
-    elif tx - nhx >= 2:
-        tx -= 1
-        ty = nhy
-    elif nhy - ty >= 2:
-        ty += 1
-        tx = nhx
-    elif ty - nhy >= 2:
-        ty -= 1
-        tx = nhx
-    return (tx, ty)
-
-
-def move(d, pos):
-    x, y = pos
-    if d == "R":
-        x += 1
-    elif d == "L":
-        x -= 1
-    elif d == "U":
-        y += 1
-    elif d == "D":
-        y -= 1
-    return (x, y)
-
-
-def simulate(d, hpos, tpos):
-    hx, hy = hpos
-    tx, ty = tpos
-    if d == "R":
-        hx += 1
-        if hx - tx >= 2:
-            tx += 1
-            ty = hy
-    elif d == "L":
-        hx -= 1
-        if tx - hx >= 2:
-            tx -= 1
-            ty = hy
-    elif d == "U":
-        hy += 1
-        if hy - ty >= 2:
-            ty += 1
-            tx = hx
-    elif d == "D":
-        hy -= 1
-        if ty - hy >= 2:
-            ty -= 1
-            tx = hx
-    return (hx, hy), (tx, ty)
-
-
 def part_a(inp):
     data = parse(inp)
-    hpos = (0, 0)
-    tpos = (0, 0)
+    chain = Chain(0, 0, 2)
     seen = set([(0, 0)])
     for d, n in data:
         for _ in range(n):
-            hpos, tpos = simulate(d, hpos, tpos)
-            seen.add(tpos)
+            chain.move(d)
+            seen.add(chain.tail_pos)
     return len(seen)
 
 
 def part_b(inp):
     data = parse(inp)
-    poss = [(0, 0) for _ in range(10)]
+    chain = Chain(0, 0, 10)
     seen = set([(0, 0)])
     for d, n in data:
         for _ in range(n):
-            nposs = []
-            pos = move(d, poss[0])
-            nposs.append(pos)
-            for lpos in poss[1:]:
-                pos = update(pos, lpos)
-                nposs.append(pos)
-            poss = nposs
-            seen.add(poss[-1])
-            print(poss)
+            chain.move(d)
+            seen.add(chain.tail_pos)
     return len(seen)
 
 
 if __name__ == "__main__":
-    with open(f"{CWD}/test.txt", "r") as f:
+    with open(f"{CWD}/test_a.txt", "r") as f:
         inp = f.read().strip()
     print("Testing  (a):", part_a(inp))
     print("Expected (a):", 13)
+    with open(f"{CWD}/test_b.txt", "r") as f:
+        inp = f.read().strip()
     print("Testing  (b):", part_b(inp))
     print("Expected (b):", 36)
     with open(f"{CWD}/input.txt", "r") as f:
         inp = f.read().strip()
     print("Actual   (a):", part_a(inp))
     print("Actual   (b):", part_b(inp))
-
-"""
-2296: too high
-"""
