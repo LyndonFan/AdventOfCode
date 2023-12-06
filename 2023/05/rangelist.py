dd8ed57ccd76a3cd1dc8bb43ab83491f0464f7d4
Lyndon Fan <lyndonfan@wessexinsights.com>
2023-12-06 12:18:34 +0000

    move rangelist to separate file, have tests

---
+++ 2023/05/rangelist.py
@@ -0,0 +1,83 @@
+class RangeList:
+    def __init__(self, ranges: list[tuple[int, int]]) -> None:
+        self.ranges = [(s, s+l) for s,l in ranges]
+    
+    def __contains__(self, x: int) -> bool:
+        return any(
+            s <= x and x < e for s, e in self.ranges
+        )
+    
+    def __len__(self) -> int:
+        return sum(e-s for s,e in self.ranges)
+    
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, RangeList):
+            return False
+        return self.ranges == other.ranges
+
+    def __repr__(self) -> str:
+        intervals_str = [f"[{s}, {e})" for s, e in self.ranges]
+        return f"RangeList({', '.join(intervals_str)})"
+
+    def intersect(self, other: "RangeList") -> "RangeList":
+        new_ranges = []
+        self_idx = 0
+        other_index = 0
+        while self_idx < len(self.ranges) and other_index < len(other.ranges):
+            self_start, self_end = self.ranges[self_idx]
+            other_start, other_end = other.ranges[other_index]
+            if self_end <= other_start:
+                self_idx += 1
+            elif self_start >= other_end:
+                other_index += 1
+            else:
+                new_start = max(self_start, other_start)
+                new_end = min(self_end, other_end)
+                new_ranges.append((new_start, new_end - new_start))
+                if self_end > other_end:
+                    other_index += 1
+                else:
+                    self_idx += 1
+        return RangeList(new_ranges)
+    
+    def union(self, other: "RangeList") -> "RangeList":
+        new_ranges = []
+        self_idx = 0
+        other_index = 0
+        while self_idx < len(self.ranges) and other_index < len(other.ranges):
+            self_start, self_end = self.ranges[self_idx]
+            other_start, other_end = other.ranges[other_index]
+            if self_end <= other_start:
+                s, e = self.ranges[self_idx]
+                new_ranges.append((s, e-s))
+                self_idx += 1
+                continue
+            elif self_start >= other_end:
+                s, e = other.ranges[other_index]
+                new_ranges.append((s, e-s))
+                other_index += 1
+                continue
+            new_start = min(self_start, other_start)
+            new_end = max(self_end, other_end)
+            while self_idx < len(self.ranges) and other_index < len(other.ranges):
+                prev_new_end = new_end
+                while self_idx < len(self.ranges) and self.ranges[self_idx][0] <= new_end:
+                    self_idx += 1
+                if self_idx == len(self.ranges):
+                    break
+                self_idx -= 1
+                new_end = max(self.ranges[self_idx][1], new_end)
+                while other_index < len(other.ranges) and other.ranges[other_index][0] <= new_end:
+                    other_index += 1
+                if other_index == len(other.ranges):
+                    break
+                other_index -= 1
+                new_end = max(other.ranges[other_index][1], new_end)
+                if new_end == prev_new_end:
+                    break
+            new_ranges.append((new_start, new_end - new_start))
+            self_idx += 1
+            other_index += 1
+        new_ranges += [(s, e-s) for s,e in self.ranges[self_idx:]]
+        new_ranges += [(s, e-s) for s,e in other.ranges[other_index:]]
+        return RangeList(new_ranges)
--- 2023/05/test_rangelist.py
+++ 2023/05/test_rangelist.py
@@ -1,5 +1,5 @@
 import pytest
-from run import RangeList
+from rangelist import RangeList
 
 def test_init():
     ranges = [(1, 5), (10, 15), (30, 25)]
@@ -16,3 +16,35 @@ def test_len():
     ranges = [(1, 5), (10, 15), (20, 25)]
     rl = RangeList(ranges)
     assert len(rl) == 5 + 15 + 25
+
+def test_intersect_no_overlap():
+    xs = RangeList([(1, 5), (10, 5)])
+    ys = RangeList([(20, 25)])
+    assert xs.intersect(ys) == RangeList([])
+
+def test_intersect_one_overlap():
+    xs = RangeList([(1, 10)])
+    ys = RangeList([(5, 10)])
+    assert xs.intersect(ys) == RangeList([(5, 6)])
+
+def test_intersect_multiple_overlap():
+    xs = RangeList([(1, 5), (10, 5)]) # [1,6) U [10,15)
+    ys = RangeList([(4, 8)]) # [4,12)
+    assert xs.intersect(ys) == RangeList([(4, 2), (10, 2)])
+
+def test_intersect_subset_equal():
+    xs = RangeList([(1, 5), (10, 5)]) # [1,6) U [10,15)
+    ys = RangeList([(1, 5)]) # [1,6)
+    assert xs.intersect(ys) == ys
+    assert ys.intersect(xs) == ys
+    assert xs.intersect(xs) == xs
+
+def test_union_no_overlap():
+    xs = RangeList([(1, 5), (10, 5)])
+    ys = RangeList([(20, 25)])
+    assert xs.union(ys) == RangeList([(1, 5), (10, 5), (20, 25)])
+
+def test_union_one_overlap():
+    xs = RangeList([(1, 10)]) # [1,11)
+    ys = RangeList([(5, 10)]) # [5,15)
+    assert xs.union(ys) == RangeList([(1, 14)])
